50572acdd7b118c48851ea7c3027b5a0b14445d1
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-09 18:29:18 +0100

    Get random Breed from a moddedRange

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -1,16 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// Use an extension of ERC721 to use setTokenURI function
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
+error RandomIpfsNft__RangeOutOfBounds();
+
+contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage {
     // When we mint an NFT, we will trigger a Chainlink VRF call to get us a random number
     // using that number, we will get a random NFT
     // We are going to separate nft based on rarity
     // Users have to pay to mint an NFT
     // The contract owner can withdraw the ETH
+
+    // Type Declarations
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
 
     // Chainlink variables
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
@@ -25,6 +35,7 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
 
     // NFT Variables
     uint256 private s_tokenCounter;
+    uint256 internal constant MAX_CHANCE_VALUE = 100;
 
     constructor(
         address vrfCoordinatorV2,
@@ -54,7 +65,29 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
         address nftOwner = s_requestIdToSender[requestId];
         uint256 newTokenId = s_tokenCounter;
+        // What does this token look like?
+        // Get number between 0 and 99
+        uint256 moddedRange = randomWords[0] % MAX_CHANCE_VALUE;
+        // 7 -> PUG | 12 -> Shiba Inu | 88 -> St. Bernard
+        Breed dogBreed = getBreedFromModdedRange(moddedRange);
         _safeMint(nftOwner, newTokenId);
+    }
+
+    function getBreedFromModdedRange(uint256 moddedRange) public pure returns (Breed) {
+        uint256 cumulativeSum = 0;
+        uint256[3] memory chanceArray = getChanceArray();
+        for (uint256 i = 0; i < chanceArray.length; i++) {
+            if (moddedRange >= cumulativeSum && moddedRange < chanceArray[i]) {
+                return Breed(i);
+            }
+            cumulativeSum = chanceArray[i];
+        }
+        revert RandomIpfsNft__RangeOutOfBounds();
+    }
+
+    // Rappresents the different chances of the different dogs
+    function getChanceArray() public pure returns (uint256[3] memory) {
+        return [10, 30, MAX_CHANCE_VALUE];
     }
 
     function tokenURI(uint256) public view override returns (string memory) {}
