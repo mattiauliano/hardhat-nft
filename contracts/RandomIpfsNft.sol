edd0eef66c5f61b441404b527f4bb03c0c517bd3
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-09 17:49:04 +0100

    Complete request and fulfill randomWords

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -5,13 +5,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract RandomIpfsNft is VRFConsumerBaseV2 {
+contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
     // When we mint an NFT, we will trigger a Chainlink VRF call to get us a random number
     // using that number, we will get a random NFT
     // We are going to separate nft based on rarity
     // Users have to pay to mint an NFT
     // The contract owner can withdraw the ETH
 
+    // Chainlink variables
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     uint64 private immutable i_subscriptionId;
     bytes32 private immutable i_keyHash;
@@ -19,12 +20,18 @@ contract RandomIpfsNft is VRFConsumerBaseV2 {
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
 
+    // VRF Helpers
+    mapping(uint256 => address) public s_requestIdToSender;
+
+    // NFT Variables
+    uint256 private s_tokenCounter;
+
     constructor(
         address vrfCoordinatorV2,
         uint64 subscriptionId,
         bytes32 keyHash,
         uint32 callbackGasLimit
-    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "RIN") {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_subscriptionId = subscriptionId;
         i_keyHash = keyHash;
@@ -40,13 +47,15 @@ contract RandomIpfsNft is VRFConsumerBaseV2 {
             i_callbackGasLimit,
             NUM_WORDS
         );
-        return requestId;
+        // Set requestId to whoever called the function
+        s_requestIdToSender[requestId] = msg.sender;
     }
 
-    function fulfillRandomWords(
-        uint256 requestId,
-        uint256[] memory randomWords
-    ) internal override {}
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        address nftOwner = s_requestIdToSender[requestId];
+        uint256 newTokenId = s_tokenCounter;
+        _safeMint(nftOwner, newTokenId);
+    }
 
-    function tokenURI(uint256) public {}
+    function tokenURI(uint256) public view override returns (string memory) {}
 }
