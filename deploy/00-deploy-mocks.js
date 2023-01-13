c5bd0521a0f44c3dd3b27abe9021a82e51cb859f
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-13 17:54:17 +0100

    Add MockV3Aggregator deploy script

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -7,17 +7,28 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     // From the docs. It costs 0.25 LINK per request
     const BASE_FEE = ethers.utils.parseEther("0.25")
     const GAS_PRICE_LINK = 1e9
-    const args = [BASE_FEE, GAS_PRICE_LINK]
+    // Adding fake decimals and initial value to pass them in the MockAggregator
+    const DECIMALS = 8
+    const INITIAL_ANSWER = 100000000000
 
     if (developmentChains.includes(network.name)) {
         log("Local network detected! Deploying mocks...")
+
         // Deploy a mock vrfCoordinator --> get it from Chainlink contracts
         await deploy("VRFCoordinatorV2Mock", {
             from: deployer,
             log: true,
             // This mock takes two parameters: baseFee and gasPriceLink
-            args: args,
+            args: [BASE_FEE, GAS_PRICE_LINK],
         })
+
+        // Deploy MockAggregator to get fake priceFeed
+        await deploy("MockV3Aggregator", {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_ANSWER],
+        })
+
         log("Mocks Deployed!")
         log("-------------------------------------------")
     }
