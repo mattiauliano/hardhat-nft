4e8f2b50ecf8e5dd9cf1771e7a8ab23e5e921333
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-13 23:50:31 +0100

    Fix tokenCounter position

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -46,8 +46,8 @@ contract DynamicSvgNft is ERC721, Ownable {
 
     function mintNft(int256 highValue) public {
         s_tokenIdToHighValue[s_tokenCounter] = highValue;
-        s_tokenCounter += 1;
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter += 1;
         emit CreatedNFT(s_tokenCounter, highValue);
     }
 
