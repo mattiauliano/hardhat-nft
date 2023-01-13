2ec041c70d5dc323ccec1692390dcd29676a23c2
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-13 23:51:04 +0100

    Test dynamicSvgNft

--- test/unit/dynamicSvgNft.test.js
+++ test/unit/dynamicSvgNft.test.js
@@ -1,6 +1,14 @@
 const { assert, expect } = require("chai")
 const { network, deployments, ethers } = require("hardhat")
 const { developmentChains } = require("../../helper-hardhat-config")
+const {
+    lowSVGImageUri,
+    highSVGimageUri,
+    lowTokenUri,
+    highTokenUri,
+    lowSVG,
+    highSVG,
+} = require("../../utils/test")
 
 !developmentChains.includes(network.name)
     ? describe.skip
@@ -18,11 +26,41 @@ const { developmentChains } = require("../../helper-hardhat-config")
               mockV3Aggregator = await ethers.getContract("MockV3Aggregator")
           })
 
-          describe("constructor", () => {})
+          describe("constructor", () => {
+              it("Sets lowSVG correctly", async () => {
+                  const lowSVG = await dynamicSvgNft.getLowSVG()
+                  assert.equal(lowSVG, lowSVGImageUri)
+              })
+              it("Sets highSVG correctly", async () => {
+                  const highSVG = await dynamicSvgNft.getHighSVG()
+                  assert.equal(highSVG, highSVGimageUri)
+              })
+              it("Sets priceFeed correctly", async () => {
+                  const priceFeed = await dynamicSvgNft.getPriceFeed()
+                  assert.equal(priceFeed, mockV3Aggregator.address)
+              })
+          })
 
-          describe("svgToImageURI", () => {})
+          describe("mintNft", () => {
+              it("emits an event and creates the NFT", async function () {
+                  const highValue = ethers.utils.parseEther("1") // 1 dollar per ether
+                  await expect(dynamicSvgNft.mintNft(highValue)).to.emit(
+                      dynamicSvgNft,
+                      "CreatedNFT"
+                  )
+                  const tokenCounter = await dynamicSvgNft.getTokenCounter()
+                  assert.equal(tokenCounter.toString(), "1")
+                  const tokenURI = await dynamicSvgNft.tokenURI(0)
+                  assert.equal(tokenURI, highTokenUri)
+              })
 
-          describe("mintNft", () => {})
-
-          describe("tokenURI", () => {})
+              it("shifts the token uri to lower when the price doesn't surpass the highvalue", async function () {
+                  // $100,000,000 dollar per ether. Maybe in the distant future this test will fail...
+                  const highValue = ethers.utils.parseEther("100000000")
+                  const txResponse = await dynamicSvgNft.mintNft(highValue)
+                  await txResponse.wait(1)
+                  const tokenURI = await dynamicSvgNft.tokenURI(0)
+                  assert.equal(tokenURI, lowTokenUri)
+              })
+          })
       })
