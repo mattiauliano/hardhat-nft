d991a98543d4bed71b8fd2ffc4ed46542fb8b434
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-13 17:55:19 +0100

    Add ethUsdPriceFeed address

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -8,6 +8,7 @@ const networkConfig = {
         keyHash: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
         subscriptionId: "8273",
         callbackGasLimit: "500000",
+        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
     },
     31337: {
         name: "hardhat",
